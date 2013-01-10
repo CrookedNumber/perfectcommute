1692508067eafcc42f3306f3129855e42fd92b75
CrookedNumber <david@crookednumber.com>
2013-01-09 23:40:23 -0500

    Made things slightly more abstract with constants; removed cruft; added inbound/outbound stuff.

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-$url = "http://developer.mbta.com/lib/RTCR/RailLine_8.json";
+<?php
+define('URL', 'http://developer.mbta.com/lib/RTCR/RailLine_8.json');
+define('TIMEZONE', 'America/New_York');
+define('DESIRED_STOP', 'West Newton');
+define('AM_DIRECTION', 'inbound');
 
-date_default_timezone_set('America/New_York');
-
-$json = file_get_contents($url);
+date_default_timezone_set(TIMEZONE);
+$json = file_get_contents(URL);
 $obj = json_decode($json);
-
-//print "<pre>" . print_r($obj, 1) . "</pre>";
 
 $trips = array();
 
@@ -14,9 +15,12 @@ foreach($obj->Messages as $messages) {
   foreach ($messages as $datum) {
     $tripstop->{$datum->Key} = $datum->Value;
   }
-  if ($tripstop->Stop == 'West Newton') {
-    $s = "<p>Train: $tripstop->Trip</p>";	
-    $s .= "<p>Stop: $tripstop->Stop</p>";
+  $direction = ($tripstop->Trip % 2) ? 'outbound' : 'inbound';
+  $merid = date('a');
+  
+  if ($tripstop->Stop == DESIRED_STOP && (($merid == 'am' && $direction == AM_DIRECTION) || ($merid = 'pm' && $direction != AM_DIRECTION))) {
+    $s  = "<p>Train: $tripstop->Trip</p>";	
+    $s .= "<p>Stop: $tripstop->Stop</p>";    
     $s .= "<p>Scheduled: " . date('h:i a', $tripstop->Scheduled) . "</p>";
     $lateness = ($tripstop->Lateness) ? $tripstop->Lateness/60 : 0;
     $emph = ($tripstop->Lateness) ? ' style="font-weight: bold;"' : '' ; 
@@ -25,14 +29,11 @@ foreach($obj->Messages as $messages) {
     $s .= "<p>Speed: $tripstop->Speed</p>";
     $lateness = ($tripstop->Lateness) ? $tripstop->Lateness/60 : 0;
     $s .= "<p>Lateness: $lateness</p>";
-    //$s .= "<p>-------</p>";
-
     $trips[$tripstop->Trip][] = $s;
   }
 }
 
 foreach ($trips as $k=>$v) {
-  //print $k . '<br>';
   foreach ($v as $vv) {
     print $vv;
   }
