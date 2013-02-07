420a1aa0406076fc78bcffa7a684b5eb55876bd4
CrookedNumber <david@crookednumber.com>
2013-02-07 00:04:53 -0500

    (Slightly) better variable names.

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ foreach($obj->Messages as $messages) {
   
   $trip_to = ($tripstop->Stop == AM_DEPARTURE && $merid == 'am' && $direction == AM_DIRECTION);
   $trip_from = ($tripstop->Stop == PM_DEPARTURE && $merid = 'pm' && $direction != AM_DIRECTION);
-  
+
   // Filter out any irrelevant stops
   // i.e., trains headed counter to your commute
   if ($trip_to || $trip_from) {
@@ -57,9 +57,9 @@ foreach($obj->Messages as $messages) {
 </head>
 <body>
 <?php
-foreach ($trips as $k => $v) {
-  foreach ($v as $vv) {
-    print $vv;
+foreach ($trips as $trip) {
+  foreach ($trip as $trip_info) {
+    print $trip_info;
   }
   print "<hr>";
 }
