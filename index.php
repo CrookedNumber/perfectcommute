7bdd24b54b39f3e309e633a0821587525bc65a5c
CrookedNumber <david@crookednumber.com>
2013-02-28 23:04:32 -0500

    Adding jQuery + jQuery mobile.

--- index.php
+++ index.php
@@ -56,6 +56,9 @@ foreach($obj->Messages as $messages) {
 ?>
 <html>
 <head>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
+<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
 </head>
 <body>
 <?php
