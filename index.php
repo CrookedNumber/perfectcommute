446c7fe4f2d888e7aada74f53c64d471e460ec90
CrookedNumber <david@crookednumber.com>
2013-03-17 11:52:47 -0400

    Adding jquery mobile pages.

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $obj = json_decode($json);
 
 $trips = array();
 
+$i = 1;
 foreach($obj->Messages as $messages) {
   $tripstop = new stdClass();
   // Turn messages into more commonsense objects
@@ -37,8 +38,10 @@ foreach($obj->Messages as $messages) {
 
   // Filter out any irrelevant stops
   // i.e., trains headed counter to your commute
+
   if ($trip_to || $trip_from) {
-    $trip_info  = '<p style="font-size: 40px;"><strong>'. date('g:i', $tripstop->Scheduled) . '</strong> will leave ';	
+    $trip_info = '<div data-role="page" id="page-' . $i . '"><div data-role="content">';
+    $trip_info  .= '<p style="font-size: 40px;" class="trip"><strong>'. date('g:i', $tripstop->Scheduled) . '</strong> will leave ';	
     $trip_info .= "$tripstop->Stop at ";    
     $lateness = ($tripstop->Lateness) ? $tripstop->Lateness/60 : 0;
     $label = ($lateness > 1) ? 'minutes' : 'minute';
@@ -50,7 +53,10 @@ foreach($obj->Messages as $messages) {
     if ($tripstop->Speed) {
       $trip_info .= " Speed: $tripstop->Speed</p>";
     }
+    $j = $i + 1;
+    $trip_info .= '<a href="#page-' . $j .'">NEXT</a></div></div>';
     $trips[$tripstop->Trip][] = $trip_info;
+    $i++;
   }
 }
 ?>
@@ -59,6 +65,9 @@ foreach($obj->Messages as $messages) {
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.css" />
 <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+<script>
+
+</script>
 </head>
 <body>
 <?php
@@ -66,7 +75,6 @@ foreach ($trips as $trip) {
   foreach ($trip as $trip_info) {
     print $trip_info;
   }
-  print "<hr>";
 }
 ?>
 </body>
